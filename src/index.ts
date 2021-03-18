63cd9fb44fc6749549861e65fc71e0dcb121ea54
keomouny <mouny.keo@hetic.net>
2021-03-18 09:50:20 +0100

    Mounybot (#32)
    * update
    
    * add setup.logger.ts
    
    * add elements for monitoring

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ require('dotenv').config(); // Recommended way of loading dotenv
 import container from "./inversify.config";
 import {TYPES} from "./types";
 import {Bot} from "./bot";
+import {logger} from "./setup.logger"
+
+
 let bot = container.get<Bot>(TYPES.Bot);
+
 bot.listen().then(() => {
-  console.log('Logged in!')
+  logger.info('Logged in!')
 }).catch((error) => {
-  console.log('Oh no! ', error)
+  logger.error('Oh no! ', error)
 });
